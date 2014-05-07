58961f3505a062c1a745e57a933443dd06e8ba23
hairy puppy <hairypuppygames@gmail.com>
2014-05-07 14:01:30 +0100

    just some text changes

--- simpleColor.lua
+++ simpleColor.lua
@@ -1,3 +1,6 @@
+-- simpleColor
+-- allows you to call up color names in lua/löve2d, instead of using the regular RGA [3 numbers] call up. by all means add to this. it would be nice to add some more.
+
 black = {0,0,0}
 white = {255,255,255}
 gray = {127,127,127}
@@ -7,3 +10,6 @@ blue = {0,0,255}
 red = {255,0,0}
 purple = {255,0,255}
 cyan = {0,255,255}
+
+-- lewis lepton
+-- smokingbunny.net
